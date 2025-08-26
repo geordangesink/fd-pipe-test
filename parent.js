0196c71ad63c771154c273baeb81cec59631b547
unknown <georgegeordan@gmail.com>
2025-08-26 13:16:43 +0200

    run with bare or pear

--- parent.js
+++ parent.js
@@ -1,15 +1,23 @@
 const path = require('bare-path')
 const { spawn } = require('bare-subprocess')
+const program = global.Bare ?? global.Pear
+
+console.log('Running from Pear:', program.argv[0].includes('pear-runtime'))
 
 const childPath = path.resolve(__dirname, 'child.js')
 
-console.log(global.Bare.argv[0])
-console.log(global.Bare.argv.slice(0))
+console.log(program.argv[0])
+console.log(program.argv.slice(0))
 console.log(childPath)
+
 // Spawn a child with a pipe at fd 3
-const child = spawn(global.Bare.argv[0], [childPath], {
-  stdio: ["inherit", "inherit", "inherit", "overlapped"]
-});
+const child = spawn(
+  program.argv[0], 
+  program.argv[0].includes('pear-runtime') ? ['run', childPath] : [childPath], 
+  {
+    stdio: ["inherit", "inherit", "inherit", "overlapped"]
+  }
+);
 
 child.stdio[3].write("hello from parent!\n");
 child.stdio[3].end();
