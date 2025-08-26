4809e6f59e666fdac177d98035949e2897b4fe55
unknown <georgegeordan@gmail.com>
2025-08-26 12:50:37 +0200

    pipe -> overlapped

--- parent.js
+++ parent.js
@@ -4,11 +4,11 @@ const { spawn } = require('bare-subprocess')
 const childPath = path.resolve(__dirname, 'child.js')
 
 console.log(global.Bare.argv[0])
-console.log(global.Bare.argv.slice(1))
+console.log(global.Bare.argv.slice(0))
 console.log(childPath)
 // Spawn a child with a pipe at fd 3
 const child = spawn(global.Bare.argv[0], [childPath], {
-  stdio: ["inherit", "inherit", "inherit", "pipe"]  // <-- this "pipe" becomes fd 3 in child
+  stdio: ["inherit", "inherit", "inherit", "overlapped"]
 });
 
 child.stdio[3].write("hello from parent!\n");
